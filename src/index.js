847785215d98f335ebb1805bd157aaf0e3ebd964
QuinWithOneN <jquinsmyth@gmail.com>
2020-01-27 14:53:44 -0600

    added slash to index

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom"
 
 
 ReactDOM.render(
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL + '/'}>
         <App />
     </Router>
     , document.getElementById('root'));
